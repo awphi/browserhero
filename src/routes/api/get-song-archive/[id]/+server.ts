53c0d007289f24a5b907deb771c26fe0edb85a12
awphi <jellymoos3@gmail.com>
2023-07-22 18:47:27 +0100

    tweaks to archive downloads

--- src/routes/api/get-song-archive/[id]/+server.ts
+++ src/routes/api/get-song-archive/[id]/+server.ts
@@ -1,5 +1,9 @@
 import type { ChorusAPISong } from "$lib/chorus";
-import { getSongArchiveStream, songMetadataKv } from "$lib/storage";
+import {
+  getSongArchiveStream,
+  saveSongArchiveToDisk,
+  songMetadataKv,
+} from "$lib/storage";
 import { error, json } from "@sveltejs/kit";
 import isEmpty from "lodash/isEmpty";
 import mime from "mime-types";
@@ -10,28 +14,28 @@ export async function GET(event) {
   const song: ChorusAPISong | undefined = await songMetadataKv.get(id);
 
   if (!song) {
-    throw error(400, `Could not find song with ID "${event.params.id}".`);
+    throw error(400, `Could not find song with specified ID.`);
   }
 
   if (song.isProcessing) {
-    return json("Currently processing the file...", { status: 202 });
-  }
-
-  if (isEmpty(song.storedObjectUrls)) {
-    song.isProcessing = true;
-    // TODO download the song archive via the gdrive API, zip it up and store it
-    // all this functionality can probably go in storage.ts
+    return json("Archive is being processed.", { status: 202 });
   }
 
   try {
-    const { stream, ext } = getSongArchiveStream(id);
-    return new Response(stream, {
-      headers: {
-        "Content-Type": mime.lookup(ext) as string,
-      },
-    });
+    const maybeStream = getSongArchiveStream(id);
+    if (maybeStream) {
+      const { stream, ext } = maybeStream;
+      return new Response(stream, {
+        headers: {
+          "Content-Type": mime.lookup(ext) as string,
+        },
+        status: 200,
+      });
+    } else {
+      saveSongArchiveToDisk(id);
+      return json("Archive is being processed.", { status: 202 });
+    }
   } catch (e: any) {
-    const msg = `Failed to load ${id} - ${e.message.toLowerCase()}`;
-    throw error(500, msg);
+    throw error(500, e.message);
   }
 }
