a2bd8d7ad8391285132096d6462126521b5b45c9
awphi <jellymoos3@gmail.com>
2023-07-30 22:13:26 +0100

    note drawing

--- src/lib/components/CanvasGuitar.svelte
+++ src/lib/components/CanvasGuitar.svelte
@@ -1,43 +1,55 @@
 <script lang="ts">
   import { activeSong } from "../stores";
   import { Guitar } from "$lib/guitar/guitar";
-  import {
-    buttonOffset,
-    buttonRadius,
-    getNoteX,
-  } from "$lib/guitar/guitar-utils";
+  import { getNoteX, type ButtonDef } from "$lib/guitar/guitar-utils";
 
   export let activeSongPoint: number;
   export let guitarWidth = 600;
+  export let buttonRadius = 55;
+  export let buttonOffset = 20;
 
   let guitar: Guitar | undefined;
   let guitarContainer: HTMLDivElement;
 
-  let buttons = [
+  let buttons: ButtonDef[] = [
     {
-      color: "#239B56",
+      color: "rgb(35,155,86)",
+      tapColor: "rgba(35,155,86, 0.5)",
     },
     {
-      color: "#E74C3C",
+      color: "rgb(231,76,60)",
+      tapColor: "rgba(231,76,60, 0.5)",
     },
     {
-      color: "#F4D03F",
+      color: "rgb(244,208,63)",
+      tapColor: "rgba(244,208,63, 0.5)",
     },
     {
-      color: "#3498DB",
+      color: "rgb(52,152,219)",
+      tapColor: "rgba(52,152,219, 0.5)",
     },
     {
-      color: "#DC7633",
+      color: "rgb(220,118,51)",
+      tapColor: "rgba(220,118,51, 0.5)",
     },
   ];
 
   $: {
+    if (guitar) {
+      guitar.destroy();
+    }
+
     if (typeof $activeSong === "object" && guitarContainer) {
-      guitar = new Guitar(guitarContainer, $activeSong.chart);
+      guitar = new Guitar(
+        guitarContainer,
+        $activeSong.chart,
+        guitarWidth,
+        buttons,
+        buttonRadius,
+        buttonOffset,
+        "ExpertSingle"
+      );
     } else {
-      if (guitar) {
-        guitar.destroy();
-      }
       guitar = undefined;
     }
   }
@@ -72,17 +84,17 @@
     bottom: ${buttonOffset}px;
   `}
     >
-      <div class="rounded-full w-4/5 aspect-square bg-base-100" />
+      <div class="rounded-full w-2/3 aspect-square bg-base-100" />
     </div>
   {/each}
 </div>
 
 <style lang="postcss">
   .guitar {
-    height: 2700px;
+    height: 2000px;
     position: absolute;
     bottom: 0;
-    //transform: perspective(900px) rotateX(45deg);
+    transform: perspective(900px) rotateX(45deg);
     transform-origin: center bottom;
     @apply bg-neutral outline outline-4 outline-base-200;
   }
