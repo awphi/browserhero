7cc9977797aa9c8df582c1c12dac510d492e48a1
awphi <jellymoos3@gmail.com>
2023-08-09 21:08:45 +0100

    input refactor

---
+++ src/lib/input-manager.ts
@@ -0,0 +1,134 @@
+import { list } from "postcss";
+
+export type Action = "strum-up" | "strum-down" | "0" | "1" | "2" | "3" | "4";
+export type ActionMap = Record<string, Action>;
+
+const actionStates = ["inactive", "pressed", "held"] as const;
+type ActionState = (typeof actionStates)[number];
+
+type ActionStateMap = {
+  [k in Action]: ActionState;
+};
+
+const defaultKeyboardKeyMap: ActionMap = {
+  "1": "0",
+  "2": "1",
+  "3": "2",
+  "4": "3",
+  "5": "4",
+  ArrowUp: "strum-up",
+  ArrowDown: "strum-down",
+};
+
+const baseActionStateMap: ActionStateMap = {
+  "strum-up": "inactive",
+  "strum-down": "inactive",
+  "0": "inactive",
+  "1": "inactive",
+  "2": "inactive",
+  "3": "inactive",
+  "4": "inactive",
+};
+
+export class InputManager {
+  private readonly keyMaps: Record<string, ActionMap>;
+  private readonly actionStateMaps: Record<string, ActionStateMap>;
+  private readonly eventListeners: [keyof WindowEventMap, Function][] = [];
+
+  // TODO use constructor parameter + svelte stores + localStorage to make rebindable per-device keymaps
+  constructor(keyMaps?: Record<string, ActionMap>) {
+    if (keyMaps && "keyboard" in keyMaps) {
+      this.keyMaps = keyMaps;
+    } else {
+      this.keyMaps = {
+        keyboard: defaultKeyboardKeyMap,
+      };
+    }
+    this.actionStateMaps = Object.fromEntries(
+      Object.keys(this.keyMaps).map((key) => [key, { ...baseActionStateMap }])
+    );
+
+    this.addEventListener("gamepadconnected", this.onGamepadConnected);
+    this.addEventListener("gamepaddisconnected", this.onGamepadDisconnected);
+    this.addEventListener("keydown", this.onKeyDown);
+    this.addEventListener("keyup", this.onKeyUp.bind(this));
+  }
+
+  private addEventListener<K extends keyof WindowEventMap>(
+    type: K,
+    listener: (ev: WindowEventMap[K]) => any,
+    options?: boolean | AddEventListenerOptions
+  ): void {
+    const boundListener = listener.bind(this);
+    window.addEventListener(type, boundListener, options);
+    this.eventListeners.push([type, boundListener]);
+  }
+
+  private setActionState(
+    controllerName: string,
+    action: Action,
+    state: ActionState
+  ): void {
+    const currentState = this.actionStateMaps["keyboard"][action];
+    if (currentState !== state) {
+      this.actionStateMaps[controllerName][action] = state;
+      console.log(controllerName, action, state);
+    }
+  }
+
+  private onKeyUp(ev: KeyboardEvent): void {
+    const keyMap = this.keyMaps["keyboard"];
+    const action = keyMap[ev.key];
+    if (action) {
+      this.setActionState("keyboard", action, "inactive");
+    }
+  }
+
+  private onKeyDown(ev: KeyboardEvent): void {
+    const keyMap = this.keyMaps["keyboard"];
+    const action = keyMap[ev.key];
+    if (action) {
+      const currentState = this.actionStateMaps["keyboard"][action];
+      this.setActionState(
+        "keyboard",
+        action,
+        currentState === "inactive" ? "pressed" : "held"
+      );
+    }
+  }
+
+  private onGamepadConnected(ev: GamepadEvent): void {
+    // note that we don't set up a default action map - we expect that to be passed in externally since there's
+    // so many different controllers. action maps can be set in the constructor or via setActionMap
+    console.log(ev, "connect");
+    this.actionStateMaps[ev.gamepad.id] = { ...baseActionStateMap };
+  }
+
+  private onGamepadDisconnected(ev: GamepadEvent): void {
+    console.log(ev, "disconnect");
+    delete this.actionStateMaps[ev.gamepad.id];
+  }
+
+  destroy(): void {
+    this.eventListeners.forEach(([key, listener]) => {
+      window.removeEventListener(key, listener as any);
+    });
+  }
+
+  setActionMap(controllerName: string, map: ActionMap): void {
+    this.keyMaps[controllerName] = map;
+  }
+
+  getActionState(action: Action): ActionState | undefined {
+    const prioInput = Math.max(
+      ...Object.values(this.actionStateMaps).map((v) =>
+        actionStates.indexOf(v[action])
+      )
+    );
+    return actionStates[prioInput];
+  }
+
+  update(): void {
+    // TODO update controllers
+  }
+}
