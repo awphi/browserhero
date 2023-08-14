2565352a3dd1bf399f561fec89bb15e0f9dba80a
awphi <jellymoos3@gmail.com>
2023-08-14 23:11:47 +0100

    tidy up song selector stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,13 +1,15 @@
 import { writable, type Writable } from "svelte/store";
 import { tweened } from "svelte/motion";
 import type { SongBundle } from "$lib/song-loader";
-import type { ChorusAPISong } from "$lib/chorus";
+import type { ChorusSongSelectorState } from "$lib/chorus";
 
-export const songSelectorSongs: Writable<ChorusAPISong[]> = writable([]);
-export const songSelectorSearchTerm: Writable<string> = writable("");
+export const songSelectorState: Writable<ChorusSongSelectorState> = writable({
+  searchTerm: "",
+  songs: [],
+  isExtendable: true,
+  extendFrom: 0,
+});
 export const activeSong: Writable<SongBundle | "loading" | undefined> =
   writable(undefined);
-export const activeSongState: Writable<"loading" | "ready"> =
-  writable("loading");
 export const activeScore: Writable<number> = tweened(0);
 export const activeCombo: Writable<number> = writable(0);
